085eedc72a1cbc9690054cf42a9dce0b9a8e384c
Renato Lobo <renatolobo_@hotmail.com>
2021-05-07 10:33:47 -0300

    Title dinâmico por página

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,16 +5,15 @@ export default class MyDocument extends Document {
     return (
       <Html >
         <Head>
-          <link rel="preconnect" href="https://fonts.gstatic.com"/>
-          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet"/>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet" />
 
-          <title>Ig.news</title>
         </Head>
-            <body>
-              <Main />
-              <NextScript />
-            </body>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
       </Html>
-          )
+    )
   }
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,16 @@
+import Head from 'next/head';
+
 import styles from '../sytles/home.module.scss';
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>
-      Hello World
-    </h1>
+    <>
+      <Head>
+        <title>Inicio | Ig.news</title>
+      </Head>
+      <h1 className={styles.title}>
+        Hello World
+      </h1>
+    </>
   )
 }
